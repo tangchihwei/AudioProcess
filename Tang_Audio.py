1ee2c6171ea189e3d2963898cd0ca5c1d9a105e3
Fred Yen <fredyen1@gmail.com>
2018-07-09 23:05:36 -0700

    major update for feature #1

--- Tang_Audio.py
+++ Tang_Audio.py
@@ -57,35 +57,77 @@ for index in range(len(snd_down) - meanFrames):
 # sound above a specific amplitude threshold. The frames that are higher than threshold
 # are defined as HIGH, otherwise as LOW.
 
-#plt.plot(snd_down)
-#plt.ylabel('amplitude')
-#plt.show()
-fig, ax = plt.subplots()
-# Plot duration of one minute
-durationSeconds = 290
+# Plot duration of desired seconds
+durationSeconds = 350
 print "Duration examined: ", durationSeconds, "seconds"
 ys = snd_down[:sampFreq/downFactor * durationSeconds]
-threshold = 0.1
-ax.axhline(y=threshold, color='r', linestyle=':')
-ax.plot(ys)
 
-# Count frames that are greater than threshold
-greater_than_threshold = [i for i, val in enumerate(ys) if val>threshold]
-# Calculate percentage
-print "Percentage of high frames: ", float(len(greater_than_threshold))*100/len(ys), "%"
+# Due to processing below, frame index will not match the sampled time since
+# a lot of unwanted frames will be discarded
 
-ax.plot(greater_than_threshold, ys[greater_than_threshold],
-        linestyle='none', color='r', marker='o', markersize=3)
-axes = plt.gca()
-axes.set_ylim([-1,1])
-plt.title("threshold set to %.2f" %threshold)
+# Remove a lot of very small non-zero values
+for idx in range(len(ys)-1):
+    if abs(ys[idx]) < 0.01:
+        ys[idx] = 0
+# Remove frames that are set to zero due to smoothing etc
+ys = ys[ys != 0]
+
+# --------------------------------------
+# Calculate window statistics, window width is roughly 2s.
+# If the percentage of frames in the window that are higher than threshold
+# is over a limit, the window is assigned as HIGH, otherwise as LOW.
+percentLimit = 0.5 # yet to be determined
+windowSize = (sampFreq/downFactor) * 2
+threshold = 0.06
+# Use a for loop or a while loop here to shift the window through the audio.
+# For each window position calculate the ratio(percent) of frames that are
+# higher than threshold.
+countArray = []
+shiftIdx = 0
+while ((shiftIdx+1)*windowSize) < len(ys):
+    count = 0
+
+    for i in range(shiftIdx*windowSize,(shiftIdx+1)*windowSize):
+
+        if ys[i] > threshold:
+            count = count + 1
+
+    countArray.append(float(count)/windowSize)
+    shiftIdx = shiftIdx + 1
+
+# Used for debugging:
+# print "performed ", shiftIdx, " shifts"
+# print "should be close to ", float(len(ys))/windowSize
+
+# Save all ratios of positions into an array.
+# List to array conversion
+countArray = np.asarray(countArray)
+# Set a boundry value to group windows into HIGHs and LOWs.
+boundryValue = 0.03
+HIGH = 0
+LOW = 0
+for winIdx in range(len(countArray)):
+    if countArray[winIdx] > boundryValue:
+        HIGH = HIGH + 1
+    else:
+        LOW = LOW + 1
+print "HIGH frame percentage is ", int(100*float(HIGH)/len(countArray)),"%"
+print "LOW frame percentage is ", int(100*float(LOW)/len(countArray)),"%"
+# --------------------------------------
+
+f, (ax1, ax2) = plt.subplots(2, 1)
+
+ax1.axhline(y=threshold, color='r', linestyle=':')
+ax1.axhline(y=threshold*(-1), color='r', linestyle=':')
+ax1.plot(ys)
+
+ax2.axhline(y=boundryValue, color='r', linestyle=':')
+ax2.plot(countArray)
+
+ax1.set_ylim([-0.5,0.5])
+ax2.set_ylim([0,0.5])
+plt.suptitle("HIGH frame percentage: %i" %(int(100*float(HIGH)/len(countArray))))
 plt.show()
-
-# Find max and min volume in clip
-# print "max is ", snd.max
-# print "min is ", snd.min
-
-# Return Time duration of HIGH and LOW frames and also present in percentage
 
 # Feature 2
 # Option to ignore parts of the audio file. Starting from the beginning and the end for custom length
